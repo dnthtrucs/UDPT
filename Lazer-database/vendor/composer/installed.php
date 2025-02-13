81c75d6471a91a9abae48dfe7481410394be387c
dnthtruc <22010313@st.phenikaa-uni.edu.vn>
2025-02-13 14:27:12 +0700

    up

---
+++ Lazer-database/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'truc/lazer-database',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'greg0/lazer-database' => array(
+            'pretty_version' => '2.0.2',
+            'version' => '2.0.2.0',
+            'reference' => '5a5be5edcbdf933466420b1dbafd5ff75ab7d7e5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../greg0/lazer-database',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'truc/lazer-database' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
